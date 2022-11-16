822de129756f2a0011404d62ba9770a5d7ec7c57
worjs <whworjs777@gmail.com>
2022-11-17 00:17:34 +0900

    add global validationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // class validator 세팅
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      skipUndefinedProperties: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
