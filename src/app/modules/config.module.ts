08d4ba537f132c63699addef5fae9fcd837254dd
worjs <whworjs777@gmail.com>
2022-11-23 18:36:53 +0900

    add env validation & install joi

--- src/app/modules/config.module.ts
+++ src/app/modules/config.module.ts
@@ -1,6 +1,14 @@
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 export const configModule = ConfigModule.forRoot({
   isGlobal: true,
   envFilePath: `.${process.env.NODE_ENV}.env`,
+  validationSchema: Joi.object({
+    NODE_ENV: Joi.string().valid('dev', 'prod', 'test', 'local'),
+    POSTGRES_USER: Joi.string().required(),
+    POSTGRES_PASSWORD: Joi.string().required(),
+    POSTGRES_DB: Joi.string().required(),
+    DATABASE_URL: Joi.string().required(),
+  }),
 });
