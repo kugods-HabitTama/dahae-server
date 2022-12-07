65a24e2761cccef2f16516ddb7acaa0ab023ef82
rimrim990 <lim990718@naver.com>
2022-12-08 01:23:03 +0900

    add create habit

--- src/habit/habit.controller.ts
+++ src/habit/habit.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { HabitService } from './habit.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateHabitPayload } from './payload/create.habit.payload';
 
+@ApiTags('Habit API')
 @Controller('habit')
-export class HabitController {}
+export class HabitController {
+  constructor(private readonly habitService: HabitService) {}
+
+  @Post('/')
+  @ApiOperation({ summary: 'create habit' })
+  async createHabit(
+    @Body() createHabitPayload: CreateHabitPayload,
+  ): Promise<void> {
+    return this.habitService.createHabit(createHabitPayload);
+  }
+}
--- src/habit/habit.service.ts
+++ src/habit/habit.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { HabitRepository } from './habit.repository';
+import { CreateHabitPayload } from './payload/create.habit.payload';
 
 @Injectable()
-export class HabitService {}
+export class HabitService {
+  constructor(private readonly habitRepository: HabitRepository) {}
+
+  async createHabit(createHabitPayload: CreateHabitPayload): Promise<void> {
+    await this.habitRepository.create(createHabitPayload);
+  }
+}
