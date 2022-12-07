f64d21562380f376ba7860a431ea71ce06e498ef
rimrim990 <lim990718@naver.com>
2022-12-07 15:04:13 +0900

    add guard test

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {Body, Controller, Post, UseGuards} from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { TestDto } from './dto/test.dto';
 import { TestPayload } from './payload/test.payload';
+import {JwtAuthGuard} from "../auth/guard/jwt-auth.guard";
 
 @ApiTags('User API')
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  //예시입니다.
-  @Post('test')
-  @ApiOperation({ summary: 'test합니다.' })
+  // jwt guard 예시입니다.
+  @Post('guard-test')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'jwt guard test 합니다.' })
   @ApiOkResponse({ type: TestDto })
   async test(@Body() payload: TestPayload): Promise<TestDto> {
     return this.userService.test(payload);
