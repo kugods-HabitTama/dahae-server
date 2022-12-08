f6e1a1c9889eaad011a287c72d8a2f8aecda1f76
rimrim990 <lim990718@naver.com>
2022-12-09 02:09:17 +0900

    add date validation

--- src/habit/habit.service.ts
+++ src/habit/habit.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { HabitRepository } from './habit.repository';
 import { CreateHabitPayload } from './payload/create.habit.payload';
 import { CreateHabitType } from './type/create.habit.type';
@@ -10,6 +10,17 @@ export class HabitService {
 
   async createHabit(createHabitPayload: CreateHabitPayload): Promise<void> {
     const { time, startDate, endDate, days } = createHabitPayload;
+
+    // startDate 검증
+    const today = new Date().toJSON().slice(0, 10);
+    if (startDate < today) {
+      throw new BadRequestException('invalid startDate');
+    }
+
+    // endDate 검증
+    if (endDate || endDate < startDate) {
+      throw new BadRequestException('invalid endDate');
+    }
 
     // 요일을 bit 합으로 변환
     const dayBit = days.reduce((acc, cur) => acc + HabitRecordDayConst[cur], 0);
