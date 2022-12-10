714aa0274c6640560d673c520ae4e72300e12020
rimrim990 <lim990718@naver.com>
2022-12-10 16:55:29 +0900

    apply jwt auth guard

--- src/habit/habit.controller.ts
+++ src/habit/habit.controller.ts
@@ -1,14 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { HabitService } from './habit.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateHabitPayload } from './payload/create.habit.payload';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 
 @ApiTags('Habit API')
 @Controller('habit')
 export class HabitController {
   constructor(private readonly habitService: HabitService) {}
 
+  @ApiBearerAuth()
   @Post('/')
+  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: 'create habit' })
   async createHabit(
     @Body() createHabitPayload: CreateHabitPayload,
