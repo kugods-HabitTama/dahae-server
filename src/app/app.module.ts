a252c9db53f1499437019d9ed0030f2a13e0d447
JaeGun Cho <whworjs777@gmail.com>
2022-12-11 16:51:52 +0900

    [Infra] logger 세팅 (#28)
    * add logger middleware
    
    * apply logger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from '../common/common.module';
 import { UserModule } from '../user/user.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { configModule } from './modules/config.module';
+import { LoggerMiddleware } from '../common/middlewares/logger.middleware';
 
 @Module({
   imports: [CommonModule, UserModule, AuthModule, configModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
