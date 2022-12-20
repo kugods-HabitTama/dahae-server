ea0bea8cfaa09fb5ff8a77e6dad07f7c99100879
worjs <whworjs777@gmail.com>
2022-12-20 21:43:10 +0900

    delete decorator about auth

--- src/habit/habit.controller.ts
+++ src/habit/habit.controller.ts
@@ -4,7 +4,6 @@ import {
   Get,
   Post,
   Req,
-  UseGuards,
   Query,
   Put,
   Delete,
@@ -14,14 +13,12 @@ import {
 } from '@nestjs/common';
 import { HabitService } from './habit.service';
 import {
-  ApiBearerAuth,
   ApiCreatedResponse,
   ApiOperation,
   ApiTags,
   ApiParam,
 } from '@nestjs/swagger';
 import { CreateHabitPayload } from './payload/create.habit.payload';
-import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { CurrentUser } from 'src/auth/decorator/user.decorator';
 import { UserInfoType } from 'src/user/types/userInfo.type';
 import { ChangeProgressPayload } from './payload/change.progress.payload';
@@ -35,9 +32,7 @@ import { UpdateHabitPayload } from './payload/update.habit.payload';
 export class HabitController {
   constructor(private readonly habitService: HabitService) {}
 
-  @ApiBearerAuth()
   @Post('/')
-  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: 'create habit' })
   async createHabit(
     @Req() req,
@@ -48,9 +43,7 @@ export class HabitController {
     return this.habitService.createHabit(id, createHabitPayload);
   }
 
-  @ApiBearerAuth()
   @Get('/')
-  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: 'get habit list' })
   @ApiCreatedResponse({
     type: GetHabitListDto,
@@ -62,9 +55,7 @@ export class HabitController {
     return this.habitService.getHabitList(id);
   }
 
-  @ApiBearerAuth()
   @Get('/record')
-  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: 'get habit records' })
   @ApiCreatedResponse({
     type: GetHabitRecordListDto,
@@ -77,9 +68,7 @@ export class HabitController {
     return this.habitService.getHabitRecords(id, query);
   }
 
-  @ApiBearerAuth()
   @Put('/')
-  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: 'change habit progress' })
   async changeProgress(
     @Body() changeProgressPayload: ChangeProgressPayload,
@@ -87,9 +76,7 @@ export class HabitController {
     return this.habitService.changeProgress(changeProgressPayload);
   }
 
-  @ApiBearerAuth()
   @Delete('/:id')
-  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: 'delete habit' })
   @ApiParam({
     name: 'id',
@@ -101,9 +88,7 @@ export class HabitController {
     return this.habitService.deleteHabit(id);
   }
 
-  @ApiBearerAuth()
   @Patch('/:id')
-  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: 'update habit' })
   async updateHabit(
     @CurrentUser() user: UserInfoType,
