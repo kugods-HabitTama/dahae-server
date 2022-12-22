a86d3f223d9489add1d2f2d3c8185ff436af7ac7
worjs <whworjs777@gmail.com>
2022-12-22 15:53:33 +0900

    add global validator option

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
       forbidUnknownValues: true,
       skipUndefinedProperties: true,
+      stopAtFirstError: true,
     }),
   );
 
